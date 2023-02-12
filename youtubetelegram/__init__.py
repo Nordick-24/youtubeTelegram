da4d3ca48aea8a6cc9586d3800781ab9021ae770
Nordick-24 <bura23610@gmail.com>
2023-02-12 15:50:51 +0200

    Version 0.1.0

--- youtubetelegram/__init__.py
+++ youtubetelegram/__init__.py
@@ -1,1 +1,10 @@
-__version__ = '0.1.0'
+import main
+import aiogram
+
+from loguru import logger
+
+logger.info("__version__ = '0.1.0'")
+
+
+if __name__ == '__main__':
+    aiogram.executor.start_polling(main.dp, skip_updates=True)
--- youtubetelegram/main.py
+++ youtubetelegram/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from pytube import YouTube
 
 from loguru import logger
@@ -20,6 +21,22 @@ async def start(message: aiogram.types.Message) -> None:
     await message.answer(f"Hello {message.from_user.first_name}, its bot for download the videos from Youtube")
 
 
-if __name__ == '__main__':
-    aiogram.executor.start_polling(dp, skip_updates=True)
+@dp.message_handler(commands=['download'])
+async def download(message: aiogram.types.Message) -> None:
+    await message.answer("Write A link")
+
+
+    @dp.message_handler()
+    async def getLink(message: aiogram.types.Message) -> None:
+        await message.reply(f"Processing for You!")
+
+
+        youtube_video = YouTube(message.text)
+        youtube_video = youtube_video.streams.get_highest_resolution()
+        youtube_video.download(output_path="video/", filename="video.mp4")
+
+        with open("video/video.mp4", 'rb') as donwloaded_video:
+            await bot.send_video(message.from_user.id, donwloaded_video)
+
+
 
